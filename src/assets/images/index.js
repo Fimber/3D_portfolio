2c35abb6555a7c61d6f3bb8f4ef690020255a214
Fimber K Elemuwa <91523017+Fimber@users.noreply.github.com>
2025-05-20 09:14:57 +0100

    first commit of full portfolio
    first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,23 @@
+import hero from './hero.jpg'
+import logo from './logo.svg'
+import log from './log.png'
+import saas from './saas.png'
+import ipe from './ipe.png'
+import refine from './refine.png'
+import site from './site.png'
+import pieces from './pieces.png'
+import scrape from './scrape.png'
+import permify from './permify.png'
+
+export {
+    hero,
+    logo,
+    log,
+    saas,
+    ipe,
+    refine,
+    site,
+    pieces,
+    scrape,
+    permify
+}
---
+++ src/constants/index.js
@@ -0,0 +1,286 @@
+import { log, saas, ipe, refine, site, pieces, scrape, permify } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+    bulb, 
+    bot,
+    rocket
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    
+    {
+        title: "Technical Writer & Editor",
+        company_name: "Logrocket",
+        icon: log,
+        iconBg: "#fbc3bc",
+        date: "June 2022 - Present",
+        points: [
+            "Authored technical articles on emerging technologies, boosting website traffic and user engagement.",
+            "Edited and improved the quality of technical content of a team of writers.",
+            "Produced in-depth tutorials praised for clarity and hands-on practicality.",
+            "Updated instruction literature based on the latest tech developments.",
+        ],
+    },
+    {
+        title: "Technical Writer",
+        company_name: "Sitepoint",
+        icon: site,
+        iconBg: "#b7e4c7",
+        date: "Oct 2022 - Dec 2022",
+        points: [
+            "Wrote blog articles on frontend and AI topics that increased traffic and engagement.",
+            "Conducted AI trend research to inform content strategy.",
+            "Worked cross-functionally to develop interactive tutorials.",
+        ],
+    },
+    {
+        title: "Technical Writer",
+        company_name: "Refine",
+        icon: refine,
+        iconBg: "#ffffff",
+        date: "Dec 2022 - Jan 2023",
+        points: [
+            "Improved article visibility using SEO strategies, achieving a 20% increase in rankings.",
+            "Collaborated on infographics that boosted engagement by 60%.",
+            "Wrote content that drove a 40% traffic boost in one month.",
+        ],
+    },
+    {
+        title: "Writer & Publication Manager",
+        company_name: "InPlainEnglish",
+        icon: ipe,
+        iconBg: "#2524fa",
+        date: "Apr 2023 - Sep 2023",
+        points: [
+            "Managed multiple Medium publications reaching over 50k readers monthly.",
+            "Authored trend-focused articles that increased reader engagement.",
+            "Improved support outcomes by crafting clearer, more helpful content.",
+            "Executed guerilla marketing on Reddit and Stack Overflow to promote articles.",
+        ],
+    },
+    {
+        title: "Freelance Technical Writer",
+        company_name: "SaaSGroup",
+        icon: saas,
+        iconBg: "#ffffff",
+        date: "Oct 2023 - Present",
+        points: [
+            "Translated complex technical topics into reader-friendly content.",
+            "Applied SEO best practices to boost content visibility and organic traffic.",
+            "Focused on Web Core Vitals and enterprise SEO requirements.",
+        ],
+    },
+    {
+        title: "Freelance Technical Writer",
+        company_name: "Permify",
+        icon: permify,
+        iconBg: "#f1f1f1",
+        date: "Aug 2024 - Jan 2025",
+        points: [
+            "Wrote in-depth content on fine-grained authorization and RBAC.",
+            "Helped grow inbound traffic by 30% through content strategy.",
+            "Boosted engagement by 40% using visual aids and SEO tactics.",
+        ],
+    },
+    {
+        title: "Freelance Technical Writer",
+        company_name: "Scrape.do",
+        icon: scrape,
+        iconBg: "#000000",
+        date: "Aug 2024 - Dec 2024",
+        points: [
+            "Authored articles on advanced web scraping techniques across multiple languages.",
+            "Produced high-performing tutorials with live code examples.",
+            "Showcased Scrape.do features in real-world use cases, increasing platform interest.",
+        ],
+    },
+    {
+        title: "Technical Editor",
+        company_name: "Pieces For Developers",
+        icon: pieces,
+        iconBg: "#000000",
+        date: "Feb 2025 - Apr 2025",
+        points: [
+            "Edited and refined technical articles for clarity, technical accuracy, and engagement.",
+            "Worked closely with developer-authors to ensure articles aligned with audience expectations.",
+            "Helped maintain the editorial quality of the Developer Playbook content series.",
+        ],
+    },
+    
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Fimber',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/fimber-elemuwa/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: bulb,
+        theme: 'btn-back-red',
+        name: 'The basics of Lighting in Unity',
+        description: 'Wrote an in-depth guide on lighting fundamentals in Unity, helping developers understand techniques like real-time lighting, baked lighting, and lightmapping.',
+        link: 'https://blog.logrocket.com/lighting-basics-unity/',
+    },
+    {
+        iconUrl: bot,
+        theme: 'btn-back-green',
+        name: 'Web Scraping Articles for Scrape.done',
+        description: 'Authored a series of advanced web scraping tutorials covering techniques in Python, Ruby, JavaScript, and PHP—focused on real-world anti-bot strategies and performance optimization.',
+        link: 'https://scrape.do/authors/fimber/',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Token-Based Authentication: A Developers Guide',
+        description: 'Authored a comprehensive guide on token-based authentication and created educative visuals, detailing its mechanisms, benefits, and best practices for securing APIs and applications.',
+        link: 'https://permify.co/post/token-based-authentication/',
+    },
+    {
+        iconUrl: rocket,
+        theme: 'btn-back-pink',
+        name: 'Technical articles for Logrocket',
+        description: 'Authored a series of technical articles on LogRocket, covering topics like CSS, React, and web development best practices, contributing to developer education and engagement.',
+        link: 'https://blog.logrocket.com/author/fimberelemuwa/',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Using Design Tokens to Theme Your SaaS App',
+        description: 'I wrote a comprehensive guide on leveraging design tokens with Saas UI and Chakra UI to efficiently theme SaaS applications, ensuring consistency and scalability across the UI.',
+        link: 'https://plainenglish.io/blog/how-to-theme-your-saas-app-with-design-tokens',
+    },
+    {
+        iconUrl: threads, // Replace with the actual Medium logo or relevant icon
+        theme: 'btn-back-gray',
+        name: 'Technical Articles on Medium',
+        description: 'My playground where I write a diverse range of technical articles, covering topics from web development and JavaScript to VPS hosting and cloud technologies, contributing to developer education and industry insights.',
+        link: 'https://medium.com/@fimber6'
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'AI Summarizer Application',
+        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+        link: 'https://aisummary.netlify.app/',
+    }
+];
